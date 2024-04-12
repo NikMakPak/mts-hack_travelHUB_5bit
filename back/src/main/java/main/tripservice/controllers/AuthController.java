75d7a5e21c083c293a7208422599a3a3215fd959
Hamst3R-23 <khudobin3@gmail.com>
2024-04-12 08:08:19 +0300

    i am tired

--- back/src/main/java/main/tripservice/controllers/AuthController.java
+++ back/src/main/java/main/tripservice/controllers/AuthController.java
@@ -1,8 +1,14 @@
 package main.tripservice.controllers;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import io.jsonwebtoken.Jwt;
 import main.tripservice.models.dto.AuthenticationRequest;
+import main.tripservice.models.response.JWTToString;
 import main.tripservice.secutiry.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,10 +26,10 @@ public class AuthController {
     private JwtService jwtService;
 
     @PostMapping("/login")
-    public String AuthenticateAndGetToken(@RequestBody AuthenticationRequest authenticationRequest){
+    public ResponseEntity<JWTToString> AuthenticateAndGetToken(@RequestBody AuthenticationRequest authenticationRequest){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getLogin(), authenticationRequest.getPassword()));
         if(authentication.isAuthenticated()){
-            return jwtService.GenerateToken(authenticationRequest.getLogin());
+            return new ResponseEntity<>(new JWTToString(jwtService.GenerateToken(authenticationRequest.getLogin())), HttpStatus.OK);
         } else {
             throw new UsernameNotFoundException("invalid user request..!!");
         }
