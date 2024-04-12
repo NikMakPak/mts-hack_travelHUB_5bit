3298b270d086972a7ec7770270869a460fde4a72
NikMakPak <nicement28@gmail.com>
2024-04-12 06:44:51 +0300

    feat: add login and routing

---
+++ front/src/store/UserStore.tsx
@@ -0,0 +1,51 @@
+import { makeAutoObservable } from 'mobx';
+import axios from 'axios';
+
+class UserStore {
+  JWT_Token = '';
+  id = 0;
+  name = '';
+  surname = '';
+  email = '';
+  password = '';
+  grade = 0;
+  passport = '';
+  bids = [];
+  squad = {};
+  role = {};
+  isAuth = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setUserData(data) {
+    this.JWT_Token = data.JWT_Token;
+    this.id = data.id;
+    this.name = data.name;
+    this.surname = data.surname;
+    this.email = data.email;
+    this.password = data.password;
+    this.grade = data.grade;
+    this.passport = data.passport;
+    this.bids = data.bids;
+    this.squad = data.squad;
+    this.role = data.role;
+  }
+
+  async login(credentials) {
+    try {
+      const response = await axios.post('/login', credentials);
+      const { data } = response;
+      this.setUserData(data);
+      localStorage.setItem('JWT_Token', data.JWT_Token);
+      this.isAuth = true;
+      return true;
+    } catch (error) {
+      console.error('Login failed:', error);
+      return false;
+    }
+  }
+}
+
+export default new UserStore();
